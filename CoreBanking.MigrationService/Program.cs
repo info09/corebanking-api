6f1486f90dde61076b609b6f00ee1b7a0ba8c77f
Quang Huy Trần <37743725+info09@users.noreply.github.com>
2025-03-10 09:36:35 +0700

    Refactor Worker and update project references
    Updated CoreBanking.MigrationService.csproj to include references to CoreBanking.Infrastructure and CoreBanking.ServiceDefaults. Modified Program.cs to add using directives, configure PostgreSQL DbContext, and add service defaults and hosted service for Worker. Refactored Worker class to include tracing, database migration, and seeding logic. Updated DistributedApplication.CreateBuilder(args) in Program.cs to use PgWeb and reformatted migrationService project reference.

--- CoreBanking.MigrationService/Program.cs
+++ CoreBanking.MigrationService/Program.cs
@@ -1,9 +1,16 @@
+using CoreBanking.Infrastructure.Data;
 using CoreBanking.MigrationService;
+using Microsoft.EntityFrameworkCore;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.AddServiceDefaults();
 builder.Services.AddHostedService<Worker>();
+
+builder.AddNpgsqlDbContext<CoreBankingDbContext>("corebanking-db", configureDbContextOptions: options =>
+{
+    options.UseNpgsql(builder => builder.MigrationsAssembly(typeof(CoreBankingDbContext).Assembly.FullName));
+});
 
 var host = builder.Build();
 host.Run();
--- CoreBanking/CoreBanking.AppHost/Program.cs
+++ CoreBanking/CoreBanking.AppHost/Program.cs
@@ -4,13 +4,12 @@ var postgres = builder.AddPostgres("postgres")
     .WithImageTag("latest")
     .WithVolume("corebanking-data", "/var/lib/postgresql/data")
     .WithLifetime(ContainerLifetime.Persistent)
-    .WithPgAdmin(builder =>
-    {
-        builder.WithImageTag("latest");
-    });
+    .WithPgWeb();
 
 var corebankingDb = postgres.AddDatabase("corebanking-db", "corebanking");
-var migrationService = builder.AddProject<Projects.CoreBanking_MigrationService>("corebanking-migrationservice").WithReference(corebankingDb).WaitFor(corebankingDb);
+var migrationService = builder.AddProject<Projects.CoreBanking_MigrationService>("corebanking-migrationservice")
+    .WithReference(corebankingDb)
+    .WaitFor(corebankingDb);
 
 builder.AddProject<Projects.CoreBanking_API>("corebanking-api")
     .WithReference(corebankingDb)
